d97cc372a7c3d4cd99dc549c6e86899928337f0f
vivek-venk <vivek.venk@gmail.com>
2017-04-18 05:25:53 -0400

    Update BasicTestIT.java

--- src/test/java/Test/BasicTestIT.java
+++ src/test/java/Test/BasicTestIT.java
@@ -16,7 +16,7 @@ public class BasicTestIT {
 	 // WebDriver	driver = new ChromeDriver();
 	 //WebDriver	driver = new FirefoxDriver();
 	 WebDriver	driver = new HtmlUnitDriver();
-	  driver.get("http://localhost:8092/retailone/");
+	  driver.get("http://54.87.168.228:8092/retailone/");
 	  Assert.assertEquals("Retail Application Demo", driver.getTitle());
 	  driver.close();
   }
