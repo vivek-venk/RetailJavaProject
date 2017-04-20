2c8e54373f2c205dc7bb7a1eb7fd51e16354f0ca
vivek-venk <vivek.venk@gmail.com>
2017-04-20 10:24:24 +0530

    Update BasicTestIT.java

--- src/test/java/Test/BasicTestIT.java
+++ src/test/java/Test/BasicTestIT.java
@@ -12,12 +12,12 @@ import org.openqa.selenium.htmlunit.*;
 public class BasicTestIT {
   @Test
   public void f() {
-	 // System.setProperty("webdriver.chrome.driver", "/bin/chromedriver");
-	 // WebDriver	driver = new ChromeDriver();
-	 //WebDriver	driver = new FirefoxDriver();
-	 //WebDriver	driver = new HtmlUnitDriver();
-	 //driver.get("http://54.87.168.228:8092/retailone/");
-	 //Assert.assertEquals("Retail Application Demo", driver.getTitle());
-	 //driver.close();
+	 System.setProperty("webdriver.chrome.driver", "/bin/chromedriver");
+	 WebDriver	driver = new ChromeDriver();
+	 WebDriver	driver = new FirefoxDriver();
+	 WebDriver	driver = new HtmlUnitDriver();
+	 driver.get("http://54.87.168.228:8080/retailone/");
+	 Assert.assertEquals("Retail Application Demo", driver.getTitle());
+	 driver.close();
   }
 }
